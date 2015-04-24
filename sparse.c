bfa1354450bd6d3ff8bea87dcf5e16fe7ad1f6a1
icecold-kv <viktor3991@gmail.com>
2015-04-24 16:40:56 +0500

    added output buffer, fixed empty sparse file

--- sparse.c
+++ sparse.c
@@ -14,20 +14,32 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	int zcnt = 0;
+	int size = 0;
 	char tmp;
+	char buf[2048];
+	int l = 0;
 	while (read(STDIN_FILENO,&tmp,1) != 0) {
+		size++;
 		if (tmp == 0) {
 			zcnt++;
+			if (l != 0) {
+				write(of, buf, l);
+				l = 0;
+			}
 		} else {
 			if (zcnt > 0) {
 				lseek(of, zcnt, SEEK_CUR);
+				zcnt = 0;
 			}
-			write(of, &tmp, 1);
-			zcnt = 0;
+			buf[l++];
+			if (l > 2048) {
+				write(of, buf, 2048);
+				l = 0;
+			}
 		}
 	}
 	if (zcnt > 0) {
-		lseek(of, zcnt, SEEK_CUR);
+		ftruncate(of,size);
 	}
 	close(of);
 	return 0;
